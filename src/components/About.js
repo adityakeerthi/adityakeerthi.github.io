073117a52e00c280f2eb6b41d6aeec8f941bdb04
Aditya <aditya.keerthi1@uwaterloo.ca>
2023-07-12 23:42:42 -0400

    update

--- src/components/About.js
+++ src/components/About.js
@@ -4,30 +4,30 @@ import Resume from '../files/resume.pdf';
 export default function About() {
     return (
         <div id="fds" class="section">
-            <p class="mb-8 text-3xl font-bold text-stone-100">
+            <p class="mb-8 text-3xl text-stone-100">
                 Aditya Keerthi 
             </p>
-            <hr class="h-px my-8 bg-stone-200 border-0"/>
+            {/* <hr class="h-px my-8 bg-stone-200 border-0"/> */}
             <p class="text-stone-300">
                 Hi, I'm Aditya and I have a dream to solve a few of the world's 
                 biggest problems. 
             </p>
             <p class="mt-4 text-stone-300"> 
                 Currently, I'm a software engineering student at the University
-                of <a class="hover:text-yellow-200 underline underline-offset-1 text-yellow-400" 
+                of <a class="hover:text-yellow-200 text-yellow-400" 
                 target="_blank" rel="noreferrer" href="https://uwaterloo.ca/">Waterloo</a>. 
-                I'll be working as a software developer at <a 
-                class="hover:text-red-200 underline underline-offset-1 text-red-400" 
-                target="_blank" rel="noreferrer" href="https://www.toyota.ca/toyota/en/">Toyota</a> this 
-                summer. I've previously interned at <a class="hover:text-purple-200 text-purple-300
-                underline underline-offset-1" target="_blank" rel="noreferrer" 
-                href="https://www.feroot.com/">Feroot</a>, a leader in client side security.
+                I'm working as a software developer at <a 
+                class="hover:text-red-200 text-red-400" 
+                target="_blank" rel="noreferrer" href="https://www.toyota.ca/toyota/en/">Toyota</a>. 
+                I've previously interned at <a class="hover:text-purple-200 text-purple-300" 
+                target="_blank" rel="noreferrer" href="https://www.feroot.com/">Feroot</a>, 
+                a leader in client side security.
             </p>
             <p class="mt-4 text-stone-300">
-                I'm looking for co-op/internship opportunities for W24.
+                I'm looking for co-op/internship opportunities for Winter 2024.
                 For more information, please check out my <a class="hover:text-white underline 
-                underline-offset-1" href={Resume}>resume</a> or shoot me an <a 
-                class="hover:text-white underline underline-offset-1" 
+                underline-offset-4" href={Resume}>resume</a> or shoot me an <a 
+                class="hover:text-white transition duration-500 hover:scale-125 underline underline-offset-4" 
                 href="mailto:adityakeerthi05@gmail.com">email</a>.
             </p>
         </div>
--- src/components/Links.js
+++ src/components/Links.js
@@ -4,15 +4,15 @@ import Resume from '../files/resume.pdf';
 export default function Links() {
     return (
         <div class="section">
-            <p class="mb-8 text-xl font font-bold text-stone-100">
+            <p class="mb-8 text-xl font text-stone-100">
                 Links
             </p>
-            <div class="flex flex-row underline underline-offset-1 text-stone-300">
-                <a class="mr-4 hover:text-white" href={Resume} rel="noreferrer" target="_blank">Resume</a>
-                <a class="mr-4 hover:text-white" href="https://www.linkedin.com/in/aditya-k1/" rel="noreferrer" target="_blank">LinkedIn</a>
-                <a class="mr-4 hover:text-white" href="https://github.com/adityakeerthi" rel="noreferrer" target="_blank">Github</a>
-                <a class="mr-4 hover:text-white" href="https://devpost.com/adityakeerthi" rel="noreferrer" target="_blank">Devpost</a>
-                <a class="mr-4 hover:text-white" href="mailto:adityakeerthi05@gmail.com/" rel="noreferrer" target="_blank">Email</a>
+            <div class="flex flex-row underline underline-offset-4 text-stone-300">
+                <a class="transition duration-500 hover:scale-125 mr-5 hover:text-white" href={Resume} rel="noreferrer" target="_blank">Resume</a>
+                <a class="transition duration-500 hover:scale-125 mr-5 hover:text-white" href="https://www.linkedin.com/in/aditya-k1/" rel="noreferrer" target="_blank">LinkedIn</a>
+                <a class="transition duration-500 hover:scale-125 mr-5 hover:text-white" href="https://github.com/adityakeerthi" rel="noreferrer" target="_blank">Github</a>
+                <a class="transition duration-500 hover:scale-125 mr-5 hover:text-white" href="https://devpost.com/adityakeerthi" rel="noreferrer" target="_blank">Devpost</a>
+                <a class="transition duration-500 hover:scale-125 mr-5 hover:text-white" href="mailto:adityakeerthi05@gmail.com/" rel="noreferrer" target="_blank">Email</a>
             </div>
         </div>
     )
