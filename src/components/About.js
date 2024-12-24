a7160a66d9b758489cee8170c76c60e0601e28fc
Aditya <adityakeerthi05@gmail.com>
2024-12-24 05:28:37 -0500

    update

--- src/components/About.js
+++ src/components/About.js
@@ -10,12 +10,13 @@ export default function About() {
             <p className="text-stone-400 transition duration-500">
                 {/* Hi, I'm Aditya and I have a dream to solve a few of the world's 
                 biggest problems.  */}
-                Hi, I'm Aditya and I'm a software engineering student at the University
+                Hi, I'm Aditya! I'm currently studying software engineering student at the University
                 of <a className="hover:text-yellow-500 transition duration-500 text-yellow-400" 
                 target="_blank" rel="noreferrer" href="https://uwaterloo.ca/">Waterloo</a>. 
-                I'm working at <a className="hover:text-blue-300 transition duration-500 text-blue-500" 
-                target="_blank" rel="noreferrer" href="https://daffodilhealth.co/">Daffodil</a> as a SWE intern. 
-                I've worked at <a className="hover:text-purple-500 transition duration-500 text-purple-300" 
+                I'm an incoming SWE Intern @ <a className="hover:text-yellow-400 transition duration-500 text-yellow-500" 
+                target="_blank" rel="noreferrer" href="https://www.rippling.com/">Rippling</a> for the next summer.
+                I've worked at <a className="hover:text-blue-300 transition duration-500 text-blue-500" 
+                target="_blank" rel="noreferrer" href="https://daffodilhealth.co/">Daffodil</a>, <a className="hover:text-purple-500 transition duration-500 text-purple-300" 
                 target="_blank" rel="noreferrer" href="https://www.feroot.com/">Feroot</a>, 
                 <a className="hover:text-green-500 transition duration-500 text-green-300" target="_blank" 
                 rel="noreferrer" href="https://www.litespace.io/"> Litespace</a>, and
@@ -24,7 +25,7 @@ export default function About() {
             </p>
 
             <p className="mt-4 text-stone-400 transition duration-500">
-                I'm looking for co-op/internship opportunities for <a className="text-indigo-50"> Summer 2025. </a>
+                I'm looking for co-op/internship opportunities during <a className="text-blue-300"> 2026. </a>
                 For more information, please feel free to shoot me an <a 
                 className="text-stone-400 hover:text-amber-100 transition duration-500 underline underline-offset-4" 
                 href="mailto:aditya.keerthi1@uwaterloo.ca">email</a>.
